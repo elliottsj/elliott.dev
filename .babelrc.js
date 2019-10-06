4d2089f15fa10c92daa9bbe921797e785acc34c4
Spencer Elliott <me@elliottsj.com>
2019-10-05 23:02:57 -0400

    Use @emotion/babel-preset-css-prop

---
+++ .babelrc.js
@@ -0,0 +1,9 @@
+/**
+ * This Babel configuration is used by Next.js to transpile TypeScript sources to JavaScript. See:
+ *  - https://github.com/zeit/next.js/tree/1a02dfd842717e690a07869c5856d0b1fc9dc726#customizing-babel-config
+ *  - https://emotion.sh/docs/@emotion/babel-preset-css-prop
+ */
+
+module.exports = {
+  presets: [require.resolve('next/babel'), require.resolve('@emotion/babel-preset-css-prop')],
+};
