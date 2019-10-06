6cc16cde6acc1073c3e2a78e4b3c35274add43f2
Spencer Elliott <me@elliottsj.com>
2019-10-05 23:03:17 -0400

    Basic layout using css grid

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,10 +1,41 @@
-import styled from '@emotion/styled';
+import { css } from '@emotion/core';
 import React from 'react';
 
-const Container = styled.div`
-  background-color: tomato;
-`;
-
-const Layout: React.FC = ({ children }) => <Container>{children}</Container>;
+const Layout: React.FC = ({ children }) => (
+  <div
+    css={css`
+      height: 100vh;
+      display: grid;
+      grid:
+        [row1-start] 'header' auto [row1-end]
+        [row2-start] 'main' 1fr [row2-end]
+        [row3-start] 'footer' auto [row3-end]
+        / auto;
+      background-color: tomato;
+    `}
+  >
+    <div
+      css={css`
+        grid-area: header;
+      `}
+    >
+      header
+    </div>
+    <div
+      css={css`
+        grid-area: main;
+      `}
+    >
+      {children}
+    </div>
+    <div
+      css={css`
+        grid-area: footer;
+      `}
+    >
+      footer
+    </div>
+  </div>
+);
 
 export default Layout;
