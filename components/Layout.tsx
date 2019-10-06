20c9fab5f97f3da35fc21e8f55386835b4e58d72
Spencer Elliott <me@elliottsj.com>
2019-10-05 23:09:40 -0400

    Use @emotion/styled for Layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,41 +1,38 @@
-import { css } from '@emotion/core';
+import Link from 'next/link';
 import React from 'react';
 
+import styled from '@emotion/styled';
+
+const Grid = styled.div`
+  height: 100vh;
+  display: grid;
+  grid:
+    [row1-start] 'header' auto [row1-end]
+    [row2-start] 'main' 1fr [row2-end]
+    [row3-start] 'footer' auto [row3-end]
+    / auto;
+`;
+
+const Header = styled.header`
+  grid-area: header;
+`;
+
+const Main = styled.main`
+  grid-area: main;
+`;
+
+const Footer = styled.footer`
+  grid-area: footer;
+`;
+
 const Layout: React.FC = ({ children }) => (
-  <div
-    css={css`
-      height: 100vh;
-      display: grid;
-      grid:
-        [row1-start] 'header' auto [row1-end]
-        [row2-start] 'main' 1fr [row2-end]
-        [row3-start] 'footer' auto [row3-end]
-        / auto;
-      background-color: tomato;
-    `}
-  >
-    <div
-      css={css`
-        grid-area: header;
-      `}
-    >
-      header
-    </div>
-    <div
-      css={css`
-        grid-area: main;
-      `}
-    >
-      {children}
-    </div>
-    <div
-      css={css`
-        grid-area: footer;
-      `}
-    >
-      footer
-    </div>
-  </div>
+  <Grid>
+    <Header>
+      <Link href="/">elliott.dev</Link>
+    </Header>
+    <Main>{children}</Main>
+    <Footer>footer</Footer>
+  </Grid>
 );
 
 export default Layout;
