31da55fcba03ee0eded0ca493e8756ce8b9292d0
Spencer Elliott <me@elliottsj.com>
2019-11-27 21:15:41 -0500

    Add Jest

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+  // Automatically clear mock calls and instances between every test
+  clearMocks: true,
+
+  // An array of file extensions your modules use
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
+
+  // Serializers for snapshot testing
+  snapshotSerializers: ['jest-emotion/serializer'],
+
+  // The pattern Jest uses to detect test files
+  testRegex: '(/__tests__/.*|(.|/)(test|spec)).(ts|tsx|js)$',
+
+  // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+
+  // A map from regular expressions to paths to transformers
+  transform: {
+    '^.+\\.(ts|tsx|js)$': require.resolve('./jest/babelTransformer'),
+  },
+};
