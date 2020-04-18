1ed3f6712d99631bc3a4d0e78326287d9c68f96f
Spencer Elliott <me@elliottsj.com>
2020-04-17 22:39:27 -0400

    Add ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+  rules: {
+    'no-unused-vars': 'off',
+    'react/prop-types': 'off',
+  },
+};
