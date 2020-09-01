e55ce063ade906ab067c10c28cfde9a95a958133
Spencer Elliott <me@elliottsj.com>
2020-08-31 20:23:15 -0400

    Use ackee

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,9 +5,10 @@ import emotionNormalize from 'emotion-normalize';
 import { ThemeProvider } from 'emotion-theming';
 import { DateTime } from 'luxon';
 import App, { AppProps } from 'next/app';
-import Router from 'next/router';
+import Router, { useRouter } from 'next/router';
 import NProgress from 'nprogress';
 import React, { useState } from 'react';
+import useAckee from 'use-ackee';
 
 import { css, Global } from '@emotion/core';
 import { config } from '@fortawesome/fontawesome-svg-core';
@@ -44,7 +45,30 @@ const mdxComponents: MDXProviderProps['components'] = {
   wrapper: Layout,
 };
 
+/**
+ * Custom hook to initialize Ackee tracking.
+ */
+const useNextAckee = () => {
+  const ackeeServerUrl = process.env.NEXT_PUBLIC_ACKEE_URL;
+  const ackeeDomainId = process.env.NEXT_PUBLIC_ACKEE_DOMAIN_ID;
+  if (!ackeeServerUrl) {
+    throw new Error(`Must define env var NEXT_PUBLIC_ACKEE_URL.`);
+  }
+  if (!ackeeDomainId) {
+    throw new Error(`Must define env var NEXT_PUBLIC_ACKEE_DOMAIN_ID.`);
+  }
+
+  const router = useRouter();
+  useAckee(
+    router.asPath,
+    { server: ackeeServerUrl, domainId: ackeeDomainId },
+    { detailed: false, ignoreLocalhost: true },
+  );
+};
+
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  useNextAckee();
+
   // Recompute the theme every second
   const [theme, setTheme] = useState(getTheme());
   useInterval(() => {
