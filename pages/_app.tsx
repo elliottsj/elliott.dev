6050d197571b4a2f57418b90105a51bebef8822f
Spencer Elliott <me@elliottsj.com>
2020-11-10 21:33:55 -0500

    add title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -86,6 +86,7 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🪐</text></svg>"
         />
+        <title>elliott.dev</title>
       </Head>
       <Global
         styles={css`
