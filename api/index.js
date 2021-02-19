29ee1c6a2d40965775fb3eb6be9e5963a8cb418c
Spencer Elliott <me@elliottsj.com>
2021-02-18 23:21:25 -0500

    let's try this

---
+++ api/index.js
@@ -0,0 +1,8 @@
+const BuiltTime = require('./built-time');
+module.exports = (req, res) => {
+  res.setHeader('content-type', 'text/plain');
+  res.send(`
+    This Serverless Function was built at ${new Date(BuiltTime)}.
+    The current time is ${new Date()}
+  `);
+};
