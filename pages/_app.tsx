4b306420a2f65ad6567a8040d9af8da17f43d1a6
Spencer Elliott <me@elliottsj.com>
2021-09-04 08:45:32 -0400

    Accept webmentions

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -135,6 +135,8 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🪐</text></svg>"
         />
+        <link rel="webmention" href="https://webmention.io/elliott.dev/webmention" />
+        <link rel="pingback" href="https://webmention.io/elliott.dev/xmlrpc" />
       </Head>
       <DefaultSeo defaultTitle="elliott.dev" titleTemplate="%s | elliott.dev" />
       <Global
