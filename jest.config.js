929ad71550b4915ed8b445c526bd332e637893ca
Spencer Elliott <me@elliottsj.com>
2023-05-21 15:30:19 -0400

    Fix Jest

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,11 @@
-// For a detailed explanation regarding each configuration property, visit:
-// https://jestjs.io/docs/en/configuration.html
+const nextJest = require('next/jest');
 
-module.exports = {
-  // Automatically clear mock calls and instances between every test
-  clearMocks: true,
+const createJestConfig = nextJest({
+  dir: './',
+});
 
-  // An array of file extensions your modules use
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
-
-  // The pattern Jest uses to detect test files
-  testRegex: '(/__tests__/.*|(.|/)(test|spec)).(ts|tsx|js)$',
-
-  // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+const customJestConfig = {
+  testEnvironment: 'jest-environment-jsdom',
 };
+
+module.exports = createJestConfig(customJestConfig);
