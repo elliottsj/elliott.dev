b5d78615757f86b17a32e65fb615b8d287460da4
Spencer Elliott <me@elliottsj.com>
2026-05-09 14:13:01 -0400

    skipLibCheck: false

---
+++ global.d.ts
@@ -0,0 +1,17 @@
+import type * as React from 'react';
+
+// Re-expose the JSX namespace globally for third-party types written against
+// the pre-React-19 typings (e.g. @types/mdx, prism-react-renderer).
+declare global {
+  namespace JSX {
+    type ElementType = React.JSX.ElementType;
+    type Element = React.JSX.Element;
+    type ElementClass = React.JSX.ElementClass;
+    type ElementAttributesProperty = React.JSX.ElementAttributesProperty;
+    type ElementChildrenAttribute = React.JSX.ElementChildrenAttribute;
+    type LibraryManagedAttributes<C, P> = React.JSX.LibraryManagedAttributes<C, P>;
+    type IntrinsicAttributes = React.JSX.IntrinsicAttributes;
+    type IntrinsicClassAttributes<T> = React.JSX.IntrinsicClassAttributes<T>;
+    type IntrinsicElements = React.JSX.IntrinsicElements;
+  }
+}
