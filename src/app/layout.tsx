3db7392432f2bd4623425a7f483ecc43f4616c67
Spencer Elliott <me@elliottsj.com>
2026-05-27 14:51:17 -0400

    no webmention

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,10 +14,6 @@ export const metadata: Metadata = {
   description: 'The personal website of Spencer Elliott',
   icons: {
     icon: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🪐</text></svg>',
-    other: [
-      { rel: 'webmention', url: 'https://webmention.io/elliott.dev/webmention' },
-      { rel: 'pingback', url: 'https://webmention.io/elliott.dev/xmlrpc' },
-    ],
   },
 };
 
